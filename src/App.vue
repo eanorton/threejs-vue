7d60a4e56b77e041d5ccbf31147b073b30b34663
eanorton <edwardnorton@icloud.com>
2019-09-01 15:09:20 -0400

    created threejs instance

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>ThreeJS:</h1>
+    <Scene />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Scene from './components/Scene';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Scene
   }
 }
 </script>
